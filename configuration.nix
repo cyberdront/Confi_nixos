99514381e2071bd5b9d9f75a023cfe1257346488
Marat <cyberdront@users.noreply.github.com>
2020-11-28 17:11:43 +0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -14,22 +14,35 @@
   nix.buildCores = 0;
   security.wrappers.slock.source = "${pkgs.slock.out}/bin/slock";
 
-  virtualisation.libvirtd.enable = true;
-  services.qemuGuest.enable = true;
   programs.adb.enable = true;
 
-  #nixpkgs.pkgs = import (builtins.fetchTarball https://github.com/nixos/nixpkgs/archive/master.tar.gz) {inherit (config.nixpkgs) config overlays; };
-  #nixpkgs.crossSystem = { system = "aarch64-linux"; config = aarch64-unknown-linux-gnu"; };
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = (pkg: true);
   nixpkgs.config.allowBroken = true;
-  nixpkgs.config.permittedInsecurePackages = ["openssl-1.0.2u"];
+  nixpkgs.config.permittedInsecurePackages = ["openssl-1.0.2u" ]; #"adobe-reader-9.5.5-1"];
+
   networking.hostName = "BStation"; # Define your hostname.
   networking.networkmanager.enable = true;
   networking.useDHCP = false;
   networking.interfaces.enp7s0.useDHCP = true;
 
+  services.teamviewer.enable = false;
+
   i18n.defaultLocale = "ru_RU.UTF-8";
   time.timeZone = "Europe/Samara";
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    liberation_ttf
+    fira-code
+    fira-code-symbols
+    mplus-outline-fonts
+    dina-font
+    proggyfonts
+    hack-font
+    nerdfonts
+  ];
 
   environment.systemPackages = with pkgs; [
     #default
@@ -38,104 +51,110 @@
     #util
     testdisk-qt slock
 
-    #bluetooth
-    bluez-tools
-
     #application
-    adobe-reader baobab dia libreoffice slack gnucash fbreader blender gimp firefox
-    tdesktop vlc pgadmin remmina protontricks
-    google-chrome chromium translate-shell
-    transmission transmission-gtk xits-math skypeforlinux
+    baobab dia libreoffice slack gnucash fbreader calibre blender gimp firefox
+    tdesktop vlc pgadmin remmina google-chrome chromium translate-shell
+    transmission-gtk vivaldi-widevine discord freecad
+    screen avidemux ffmpeg-full lame
+    cudatoolkit
+
+    #wine
+    steam winetricks protontricks steam-run steamcmd #linux-steam-integration
+    steam-run playonlinux
+    sc-controller #steamcontroller 
+    
+    # support both 32- and 64-bit applications
+    #wineWowPackages.stable
+    # support 32-bit only
+    wine
+    # support 64-bit only
+    # (wine.override { wineBuild = "wine64"; })
+    # wine-staging (version with experimental features)
+    # wineWowPackages.staging
+
+    #games
+    #minecraft minecraft-server multimc
 
     #sound maker
     audacity ardour FIL-plugins lmms mixxx
 
+    #develop
+    atom #android-studio vscodium
+    jetbrains.pycharm-community
+    #ghcid
+
     #language
-    clisp sbcl clojure mono stack cabal-install cmake
-    gcc ghc atom clang gitAndTools.gitFull llvm autobuild autoconf automake binutils
-    coreutils-full
-    #monodevelop unity3d
+    clisp sbcl gcc cmake clang llvm autobuild autoconf automake binutils
+    mono gitAndTools.gitFull coreutils-full
+
+    #haskell
+    ghc ghcid stack cabal-install
 
     #tools
-    mtpfs jmtpfs slock pavucontrol microcodeIntel ntfs3g btrfs-progs zfs zfstools firmwareLinuxNonfree
-    gparted psmisc bcache-tools debianutils p7zip
+    mtpfs jmtpfs pavucontrol microcodeIntel ntfs3g btrfs-progs zfs zfstools firmwareLinuxNonfree
+    gparted psmisc bcache-tools debianutils p7zip unzip unrar gzip zstd
     drive
 
-    #develop
-    jetbrains.pycharm-community android-studio vscodium
-
-    #game
-    steam (wine.override { wineBuild = "wineWow"; })
-    steam-run steam-run-native playonlinux samba
-
+    msbuild #monodevelop unity3d aqbanking
+    xits-math skypeforlinux
     #yandex-disk
 
     #fonts
-    hack-font
-
-    #emul
-    #anbox
+    #hack-font
 
     #python
-    python3
-    python37
     python38
-    python
-    #python37Packages.binwalk
-    #python37Packages.folium python37Packages.pandas python37Packages.geojson
-    #python37Packages.geopandas python37Packages.pgsanity python37Packages.six
-    #python37Packages.virtualenv python37Packages.markupsafe python37Packages.jinja2
-    #python37Packages.branca python37Packages.numpy python37Packages.requests
-    #python37Packages.urllib3 python37Packages.pytz python37Packages.dateutil
-    #python37Packages.chardet python37Packages.certifi python37Packages.idna
-    #python37Packages.pycairo python37Packages.django
-    #python37Packages.flask
-    #python38Packages.binwalk
-    #conda pew
+    python38Packages.virtualenv
+    python38Packages.pip python38Packages.pip-tools #python37Packages.pip2nix
+    python38Packages.pipdate
+    python38Packages.pyudev python38Packages.evdev
+    python38Packages.ds4drv
 
     #library
-    musl libunwind
-    x264 openh264 gstreamer pitivi
+    #musl libunwind lit lld wllvm
+    x264 openh264 #gstreamer
 
-    #mobile-nixos
-    mesa wayland wayland-protocols pkg-configUpstream pkg-config
-    meson
+    #mesa wayland wayland-protocols pkg-configUpstream pkg-config meson
   ];
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  hardware.steam-hardware.enable = true;
+
   # Bluetooth
   services.blueman.enable = true;
   hardware.bluetooth.enable = true;
   hardware.bluetooth.package = pkgs.bluezFull;
-
+  
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
   hardware.pulseaudio.package = pkgs.pulseaudioFull;
   hardware.pulseaudio.support32Bit = true;
-
+  hardware.pulseaudio.extraModules = [ pkgs.pulseaudio-modules-bt ];
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.layout = "us,ru(winkeys)";
   services.xserver.xkbOptions = "grp:ctrl_shift_toggle";
-  services.xserver.videoDrivers = [ "modesetting" "nvidia" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
+
   hardware.opengl = {
     enable = true;
     driSupport32Bit = true;
+    extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
   };
 
   services.xserver.desktopManager.xfce.enable = true;
   services.xserver.displayManager.defaultSession = "xfce";
-
-  services.compton.enable = true;
-  services.compton.fade = true;
-  services.compton.inactiveOpacity = "0.9";
-  services.compton.shadow = true;
-  services.compton.fadeDelta = 4;
-
-  #services.samba.enable = true;
+  #services.xserver.desktopManager.xfce.thunarPlugins = [];
+  services.picom = {
+    enable          = true;
+    fade            = true;
+    inactiveOpacity = 0.9;
+    shadow          = true;
+    fadeDelta       = 4;
+  };
 
   system.stateVersion = "20.09";
   system.autoUpgrade.enable = true;
@@ -161,5 +180,6 @@
     automatic = true;
     dates = [ "20:00" ];
   };
+
 
 }
